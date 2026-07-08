1125cf93062ff049db5d0fc4175b8454e50b3c2f
ChemmicalEiEi <s68319010007@kktech.ac.th>
2026-07-08 14:27:41 +0700

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,6 @@
 ﻿import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' 
 
 export default defineConfig({
-  plugins: [react()],
   preview: {
     host: true,
     allowedHosts: ['aware-insight-production-34a7.up.railway.app'],
